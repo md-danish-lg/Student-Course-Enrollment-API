9596f78043e10cf2fc8e5c30622d6effb5c90827
md-danish-lg <dk665226@gmail.com>
2026-06-23 13:12:32 +0500

    start

---
+++ src/main/java/com/danish/course_enrollment/StudentCourseEnrollmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.danish.course_enrollment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCourseEnrollmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCourseEnrollmentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danish/course_enrollment/StudentCourseEnrollmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danish.course_enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCourseEnrollmentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
